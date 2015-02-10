d8f058901b1c53b809624e6df7bcce87df014cc7
Chris Moy <chris@wetbar.co>
2015-02-10 15:35:35 -0600

    Add Pretty Everything

---
+++ app/models/vote.rb
@@ -0,0 +1,6 @@
+class Vote < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :votable, polymorphic: true
+
+  # validates :post_id, uniqueness: {scope: :user_id}
+end
---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :title, :url, :text, :thumbnail_url, :user_id, :created_at, :updated_at

4a4ddb5884e063fd86b09c10aeab5fd0e3ed59ef
Chris Moy <chris@wetbar.co>
2015-02-12 10:10:48 -0600

    vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :votable, polymorphic: true
 
-  # validates :post_id, uniqueness: {scope: :user_id}
+  validates :post_id, uniqueness: {scope: :user_id}
 end

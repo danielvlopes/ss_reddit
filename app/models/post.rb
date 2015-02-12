098fd4ca4d0bf398e2bfdb65d37a78acc61d5be8
Wade Novak <wade.novak@gmail.com>
2015-02-12 10:45:42 -0600

    Fix merge conflicts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,4 @@
 class Post < ActiveRecord::Base
-<<<<<<< HEAD
-  belongs_to :user
-  has_many :votes, :as => :votable
-=======
   belongs_to :author, foreign_key: :user_id, class_name: "User"
   has_many :votes, as: :votable
->>>>>>> 690a82d0333f0ef4ac3b1a47a9ea5fa6686cdd01
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,9 +1,6 @@
 class Vote < ActiveRecord::Base
-<<<<<<< HEAD
   belongs_to :user
   belongs_to :votable, :polymorphic => true
-=======
-  belongs_to :votable, polymorphic: true
-  belongs_to :user
->>>>>>> 690a82d0333f0ef4ac3b1a47a9ea5fa6686cdd01
+  validates :user, :presence => true
+  validates :votable, :presence => true
 end
